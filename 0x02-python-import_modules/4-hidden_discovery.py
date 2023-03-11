fd57c147f14b03cc2ff84598a26f2e15c492007f
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-03-11 20:33:56 +0300

    Create 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    
+    import hidden_4
+    lists = dir(hidden_4)
+    for i in range(len(lists)):
+        for j in range(len(lists[i])):
+            if (lists[i][j] == '_' and lists[i][j+1] == '_'):
+                break
+            else:
+                print(lists[i])
+                break

b954418758404f639ff78ece511f957e7e319a11
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-03-11 20:57:23 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,21 @@ if __name__ == "__main__":
     '''handles basic operations'''
     import sys
     from calculator_1 import add, sub, mul, div
-    
-    if len(sys.argv) != 4:
+    args = sys.argv
+    if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and sys.argv[2] != '/':
+    if args[2] != '+' and args[2] != '-' and args[2] != '*' and args[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] == '+':
+        print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
+    elif args[2] == '-':
+        print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
+    elif args[2] == '*':
+        print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
+    elif args[2] == '/':
+        print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
+

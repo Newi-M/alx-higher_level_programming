cebff5b684515b948a2629ad204fadff2478fd4d
Newi-M <nawwal.moon77@gmail.com>
2023-03-13 01:37:46 -0700

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    '''Finds all multiples of 2 in a list'''
+    new = []
+    for i in my_list:
+        if i % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return new
+

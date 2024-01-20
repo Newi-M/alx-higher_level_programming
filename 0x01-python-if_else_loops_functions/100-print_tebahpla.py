bbb2bb17fea9b03b3083c393a96ad32fab03793a
Newi-M <nawwal.moon77@gmail.com>
2024-01-20 10:28:48 -0800

    Upper case and lower case alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for j in (122, 96, -1):
-    if (j % 2) == 0:
-        i = 0
-    else:
-        i = 32
+i = 0
+for j in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(j - i)), end="")
+    i = 32 if i == 0 else 0

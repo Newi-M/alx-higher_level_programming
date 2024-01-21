0c5ff7be710970fd566d728b8cfd7ef6d47c0ee8
Newi-M <nawwal.moon77@gmail.com>
2024-01-21 00:17:09 -0800

    Arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
 
-    import sys
-
     l = len(sys.argv)
+    if l == 2:
+        print("1 argument:")
+    elif l == 1:
+        print("0 arguments.")
+        sys.exit()
+    else:
+        print("{} arguments:".format(l - 1))
+    
     for i in range(l):
-        if l == 2:
-            print("1 argument:")
-        elif l == 1:
-            print("0 arguments.")
-            break
-        else:
-            print("{} arguments:".format(l - 1))
-        print("{}: {}".format(i + 1, sys.argv[i + 1])
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

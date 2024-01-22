1ee307b67c0b12939806c8f30d4e24fb5c5d6320
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2024-01-22 19:38:32 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == "__main__":
+    import sys
+
     l = len(sys.argv) - 1
     if l == 0:
         print("0 arguments.")
@@ -10,6 +10,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(l))
-    
     for i in range(l):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))

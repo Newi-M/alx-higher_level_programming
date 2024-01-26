450e6ed71b787a960a602c506161698fe776cb76
Newi-M <nawwal.moon77@gmail.com>
2024-01-26 08:27:19 -0800

    Add unique values

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    
+    my_set = set(my_list)
+    new_list = list(my_set)
+    sum = 0
+
+    for i in new_list:
+        sum += i
+    
+    return sum

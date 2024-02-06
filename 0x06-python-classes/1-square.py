cc25e2ed2190102d07e8cd9fb8ac74817e6fdbae
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2024-02-06 20:06:10 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-"""class Square."""
+"""Class Square."""
 
 
 class Square:
-   
+    """Represent a square."""
+
     def __init__(self, size):
-        
+        """Initialize Square.
+
         Args:
             size (int): 
-      
+            
         self.__size = size

89e38dd50808ab910b4123a4a909362df7548e70
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2024-04-16 15:17:15 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a function that returns the JSON representation of an object (string)"""
+"""Defines a function that returns the JSON representation 
+of an object (string)"""
 import json
 
 
